fbb6a013d34812e4409cf4d0cf569688e14ee5a0
KrissLee <956140921@qq.com>
2022-09-27 15:34:26 +0800

    调整trace类，等待实际测试

--- WS_MapIDRec/MainModule.cs
+++ WS_MapIDRec/MainModule.cs
@@ -17,75 +17,90 @@ namespace WS_MapIDRec
     [BepInPlugin("com.nekoice.MapIDRecord", "MapIDRecord", "0.0.1")]
     public class WS_MapIDRec : BaseUnityPlugin
     {
+        delegate void CheckValueChangeHandle();
+        event CheckValueChangeHandle GoCheckValue;
         void Start()
         {
             Logger.LogInfo("Starting MapID Record");
-
+            InstanceFieldTrace MapIDTrace = new InstanceFieldTrace(typeof(StageControl), nameof(StageControl.instance), nameof(StageControl.instance.MapID));
+            MapIDTrace.OnValueChange += MapIDTrace_OnValueChange;
+            GoCheckValue += MapIDTrace.CheckValChange;
 
             Logger.LogInfo("MapID Record is Enabled");
         }
 
-        private void Trace_OnValueChange(object val)
+        private void MapIDTrace_OnValueChange(object val)
         {
-            Logger.LogInfo(val);
+            
         }
 
-        int lastmapidval = 0;
-        int laststageval = 0;
         void Update()
         {
-            if(StageControl.instance != null)
-            {
-                int val = StageControl.instance.MapID;
-                int stageval = StageControl.instance.stageMapId;
-                if (val != lastmapidval)
-                {
-                    Trace_OnValueChange(("MapID: ", val, "StagemapID: ", stageval, "SceneName:", SceneManager.GetActiveScene().name));
-                }
-                lastmapidval = val;
-                laststageval = stageval;
-            }
-            
+            GoCheckValue();
         }
         
     }
-    public class InstanceFieldTrace<T>
+    public class InstanceFieldTrace<T> : InstanceFieldTrace
+    {
+        new public delegate void ValueChangeHandle(T val);
+        new public event ValueChangeHandle OnValueChange;
+        T _val;
+        T _lastVal;
+        public InstanceFieldTrace(Type instanceType, string instanceName, string fieldName) : base(instanceType, instanceName, fieldName)
+        {
+            _instanceName = instanceName;
+            _target = instanceType;
+            _fieldName = fieldName;
+            var instance = _target.GetField(_instanceName)
+                                  .GetValue(null);
+            _val = (T)instance.GetType()
+                              .GetRuntimeField(fieldName)
+                              .GetValue(instance);
+            _lastVal = _val;
+        }
+        new public void CheckValChange()
+        {
+            var instance = _target.GetField(_instanceName)
+                                  .GetValue(null);
+            if (instance == null) return;
+            _val = (T)instance.GetType()
+                              .GetRuntimeField(_fieldName)
+                              .GetValue(instance);
+            if (_val.GetHashCode() != _lastVal.GetHashCode())
+            {
+                OnValueChange(_val);
+                _lastVal = _val;
+            }
+        }
+    }
+    public class InstanceFieldTrace
     {
         public delegate void ValueChangeHandle(object val);
         public event ValueChangeHandle OnValueChange;
-        Type _target;
-        string _fieldName;
-        T _val;
-        T _lastVal;
-        public InstanceFieldTrace(object parentInstance, string propertyName, ValueChangeHandle callback)
+        protected Type _target;
+        protected string _fieldName;
+        protected string _instanceName;
+        object _val;
+        object _lastVal;
+        public InstanceFieldTrace(Type instanceType, string instanceName, string fieldName)
         {
-            _target = parentInstance.GetType();
-            _fieldName = propertyName;
-            var instance = _target.GetField("instance")
+            _instanceName = instanceName;
+            _target = instanceType;
+            _fieldName = fieldName;
+            var instance = _target.GetField(_instanceName)
                                   .GetValue(null);
-            _val = (T)instance.GetType()
-                              .GetRuntimeField(propertyName)
-                              .GetValue(instance);
-            _lastVal = _val;
-            OnValueChange += callback;
-        }
-        public InstanceFieldTrace(object parentInstance, string propertyName)
-        {
-            _target = parentInstance.GetType();
-            _fieldName = propertyName;
-            var instance = _target.GetField("instance")
-                                  .GetValue(null);
-            _val = (T)instance.GetType()
-                              .GetRuntimeField(propertyName)
+            _val = instance.GetType()
+                              .GetRuntimeField(fieldName)
                               .GetValue(instance);
             _lastVal = _val;
         }
+
         public void CheckValChange()
         {
-            if (_target == null) return;
-            var instance = _target.GetField("instance")
+            var instance = _target.GetField(_instanceName)
                                   .GetValue(null);
-            _val = (T)instance.GetType()
+            if (instance == null) return;
+            _val = instance.GetType()
                               .GetRuntimeField(_fieldName)
                               .GetValue(instance);
             if (_val.GetHashCode() != _lastVal.GetHashCode())
